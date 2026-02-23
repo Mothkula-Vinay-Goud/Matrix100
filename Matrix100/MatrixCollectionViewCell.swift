35ac4620c43f9107660eba65ffe76faa40a6cd9f
Vinay <mothkulavinaygoud@gmail.com>
2026-02-23 13:24:34 -0600

    tint colour changed

--- Matrix100/MatrixCollectionViewCell.swift
+++ Matrix100/MatrixCollectionViewCell.swift
@@ -12,6 +12,7 @@ class MatrixCollectionViewCell: UICollectionViewCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .white
         return label
     }()
     
